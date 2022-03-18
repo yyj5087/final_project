796b114d714b258ccfa7404bd4dc23e900fe27e2
win10 <naki5087@naver.com>
2022-03-18 19:03:53 +0900

    #6 회원가입 성공일 경우 토스트 띄우기 추가

--- app/src/main/java/fastcampus/aop/part2/final_project/SignUpActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/SignUpActivity.kt
@@ -2,8 +2,13 @@ package fastcampus.aop.part2.final_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import fastcampus.aop.part2.final_project.databinding.ActivitySignUpBinding
+import fastcampus.aop.part2.final_project.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : BaseActivity() {
     lateinit var binding: ActivitySignUpBinding
@@ -19,7 +24,31 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvent() {
 
+        binding.btnSignUP.setOnClickListener {
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+            val inputName = binding.edtName.text.toString()
+            val inputPhoneNum = binding.edtPhoneNum.text.toString()
 
+            apiList.putRequestSignUp(inputEmail, inputPassword, inputName, inputPhoneNum).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful){
+                        Toast.makeText(mContext, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
+
+
+        }
     }
 
     override fun setupValues() {
--- app/src/main/java/fastcampus/aop/part2/final_project/api/APIList.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/api/APIList.kt
@@ -3,9 +3,11 @@ package fastcampus.aop.part2.final_project.api
 import fastcampus.aop.part2.final_project.datas.BasicResponse
 import org.json.JSONObject
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface APIList {
 
@@ -14,5 +16,14 @@ interface APIList {
     fun postRequestLogin(
         @Field("email") email: String,
         @Field("password") pw: String
+    ) : Call<BasicResponse>
+
+    @FormUrlEncoded
+    @PUT("/user")
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+        @Field("name") name: String,
+        @Field("phone") phone: String
     ) : Call<BasicResponse>
 }

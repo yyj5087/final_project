57eef7e8bbf646dc6db63428b0640909d75adead
win10 <naki5087@naver.com>
2022-03-18 17:27:26 +0900

    #5 인터페이스에, 로그인 기능을 위한 함수 명세 작성.

--- app/src/main/java/fastcampus/aop/part2/final_project/api/APIList.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/api/APIList.kt
@@ -1,4 +1,15 @@
 package fastcampus.aop.part2.final_project.api
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.POST
+
 interface APIList {
+
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password") pw: String
+    ) : Call<JSONObject>
 }

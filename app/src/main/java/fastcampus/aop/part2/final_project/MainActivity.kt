c68eb0f3412a7b2c1383fee928256164ffe34efa
yumi5087 <72373763+yumi5087@users.noreply.github.com>
2022-03-18 10:50:53 +0900

    #2 데이터바인딩 세팅 마무리

--- app/src/main/java/fastcampus/aop/part2/final_project/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/MainActivity.kt
@@ -2,10 +2,15 @@ package fastcampus.aop.part2.final_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import fastcampus.aop.part2.final_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }

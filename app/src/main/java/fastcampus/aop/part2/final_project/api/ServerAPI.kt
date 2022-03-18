f8a6f710b3977e0ee8e05efaab8048a6f458a9d9
win10 <naki5087@naver.com>
2022-03-18 17:00:25 +0900

    #1 실제 통신 담당 Retrofit 객체를 하나만 만들어서 관리하는 함수 생성

--- app/src/main/java/fastcampus/aop/part2/final_project/api/ServerAPI.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/api/ServerAPI.kt
@@ -1,9 +1,20 @@
 package fastcampus.aop.part2.final_project.api
 
+import retrofit2.Retrofit
+
 class ServerAPI {
 
     companion object{
 
+        private var retrofit : Retrofit? = null
 
+        fun getRetrofit() : Retrofit {
+
+            if (retrofit == null){
+                retrofit = Retrofit.Builder()
+                    .build()
+            }
+            return retrofit!!
+        }
     }
 }

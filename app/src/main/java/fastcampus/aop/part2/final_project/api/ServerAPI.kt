47c25ee0aa840628695c3e73f85ad2d480bad6f7
win10 <naki5087@naver.com>
2022-03-18 17:12:29 +0900

    #5 ServerAPI 생성  - Retrofit 기반

--- app/src/main/java/fastcampus/aop/part2/final_project/api/ServerAPI.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/api/ServerAPI.kt
@@ -7,11 +7,13 @@ class ServerAPI {
     companion object{
 
         private var retrofit : Retrofit? = null
+        private val BASE_URL = "https://api.copang.xyz"
 
         fun getRetrofit() : Retrofit {
 
             if (retrofit == null){
                 retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
                     .build()
             }
             return retrofit!!

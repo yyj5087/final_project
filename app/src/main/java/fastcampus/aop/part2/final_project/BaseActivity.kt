01ed1303c3e5e918dd5983cc89097747e4530cb5
win10 <naki5087@naver.com>
2022-03-29 16:00:27 +0900

    #9 사용자 프로필 오류

--- app/src/main/java/fastcampus/aop/part2/final_project/BaseActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/final_project/BaseActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import fastcampus.aop.part2.final_project.api.APIList
 import fastcampus.aop.part2.final_project.api.ServerAPI
 
@@ -24,7 +26,13 @@ abstract class BaseActivity: AppCompatActivity() {
 
         val retrofit = ServerAPI.getRetrofit(mContext)
         apiList = retrofit.create(APIList::class.java)
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
     }
+
+
 
     override fun setTitle(title: CharSequence?) {
 
@@ -34,4 +42,16 @@ abstract class BaseActivity: AppCompatActivity() {
 
     abstract fun setupEvent()
     abstract fun setupValues()
+
+    private fun setCustomActionBar(){
+        val CustomActionBar = supportActionBar!!
+        CustomActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        CustomActionBar.setCustomView(R.layout.custom_action_bar)
+        val toolbar = CustomActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
+
+        val customBar = CustomActionBar.customView
+        txtTitle = customBar.findViewById(R.id.txtTitle)
+        btnAdd = customBar.findViewById(R.id.btnAdd)
+    }
 }
